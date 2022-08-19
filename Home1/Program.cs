95361f178928b087a6b2ae4e8b11755f5994a58d
Dmitry-Lab <poznavshii@gmail.com>
2022-08-19 11:01:29 +0400

    done

---
+++ Home1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = a;
+
+if (b > max) max = b;
+if (a == max) min = b;
+
+Console.WriteLine(" ");
+Console.Write("Большее число = ");
+Console.WriteLine(max);
+
+Console.Write("Меньшее число = ");
+Console.WriteLine(min);
